2e220f70551f3270567b473fab941dfa0becfe89
Paras Agrawal <145145287+parasag1310@users.noreply.github.com>
2024-10-10 22:29:27 +0530

    Add files via upload

---
+++ DAY-13 EXPERT.py
@@ -0,0 +1,8 @@
+# cook your dish here
+# cook your dish here
+for i in range(int(input())):
+    x,y=map(int,input().split())
+    if(y>=(x/2)):
+        print("YES")
+    else:
+        print("NO")
