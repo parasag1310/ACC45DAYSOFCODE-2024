0e8d77ce604f85ad47922fa5db00a37812b9e8b9
Paras Agrawal <145145287+parasag1310@users.noreply.github.com>
2024-10-24 22:05:32 +0530

    Add files via upload

---
+++ Day-22 DPOLY.py
@@ -0,0 +1,9 @@
+# cook your dish here
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    for i in range(n-1, -1, -1):
+        if a[i] != 0:
+            print(i)
+            break# cook your dish here
